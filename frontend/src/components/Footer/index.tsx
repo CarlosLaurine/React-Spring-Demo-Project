841b3f81772d0d3519f3142b25d824c5ba27e6f5
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-05-05 02:35:08 -0300

    Basic Static Components Implemented (NavBar + Footer + DataTable)

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App Developed By <a href="https://github.com/CarlosLaurine" target="_blank" rel="noreferrer">Carlos Laurine</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
