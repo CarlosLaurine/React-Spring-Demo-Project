9c1a217e08e04c182756a8109bc25d56cad24dd4
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-05-12 22:53:22 -0300

    BASE_URL Fixed (Final)

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://spring-react-sales-laurinedev.herokuapp.com' ?? 'http://localhost:8080'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://spring-react-sales-laurinedev.herokuapp.com' ??'http://localhost:8080'
